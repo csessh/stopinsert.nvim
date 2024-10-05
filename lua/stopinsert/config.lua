1bc2d3de5ce4274463e57cab98af52578a84d5c8
Thang Do <csessh@hey.com>
2024-10-05 21:15:48 +0930

    Update linter/formatter rules to match LuaRocks standards
    3 space tab instead of 4

--- lua/stopinsert/config.lua
+++ lua/stopinsert/config.lua
@@ -1,24 +1,24 @@
 local M = {}
 
 M.config = {
-    idle_time_ms = 5000,
-    disabled_filetypes = {
-        "TelescopePrompt",
-        "checkhealth",
-        "help",
-        "lspinfo",
-        "mason",
-        "neo%-tree*",
-        "dashboard",
-    },
+   idle_time_ms = 5000,
+   disabled_filetypes = {
+      "TelescopePrompt",
+      "checkhealth",
+      "help",
+      "lspinfo",
+      "mason",
+      "neo%-tree*",
+      "dashboard",
+   },
 }
 
 ---@param user_config table
 ---@return nil
 function M.set(user_config)
-    for key, value in pairs(user_config) do
-        M.config[key] = value
-    end
+   for key, value in pairs(user_config) do
+      M.config[key] = value
+   end
 end
 
 return M
--- lua/stopinsert/init.lua
+++ lua/stopinsert/init.lua
@@ -4,35 +4,35 @@ local timer = nil
 
 ---@return nil
 local function force_exit_insert_mode()
-    vim.cmd("stopinsert")
+   vim.cmd("stopinsert")
 end
 
 ---@return nil
 local function reset_timer()
-    if timer then
-        timer:stop()
-    end
-    timer = vim.defer_fn(force_exit_insert_mode, config.idle_time_ms)
+   if timer then
+      timer:stop()
+   end
+   timer = vim.defer_fn(force_exit_insert_mode, config.idle_time_ms)
 end
 
 ---@param user_config table
 ---@return nil
 function M.setup(user_config)
-    user_config = user_config or {}
-    require("stopinsert.config").set(user_config)
+   user_config = user_config or {}
+   require("stopinsert.config").set(user_config)
 
-    vim.api.nvim_create_autocmd("InsertEnter", {
-        callback = function()
-            reset_timer()
-        end,
-    })
+   vim.api.nvim_create_autocmd("InsertEnter", {
+      callback = function()
+         reset_timer()
+      end,
+   })
 
-    vim.on_key(function(_, _)
-        if vim.fn.mode() ~= "i" then
-            return
-        end
-        reset_timer()
-    end)
+   vim.on_key(function(_, _)
+      if vim.fn.mode() ~= "i" then
+         return
+      end
+      reset_timer()
+   end)
 end
 
 return M
