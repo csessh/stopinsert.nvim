406e21a01b4db7329580037a169bab5f1b66ecf5
Thang Do <csessh@hey.com>
2025-06-05 09:53:20 +0700

    Fix timer handling when disabling plugin

--- lua/stopinsert/util.lua
+++ lua/stopinsert/util.lua
@@ -21,10 +21,19 @@ function M.force_exit_insert_mode()
 end
 
 ---@return nil
-function M.reset_timer()
+function M.clear_timer()
    if timer then
       timer:stop()
+      if timer.close then
+         timer:close()
+      end
+      timer = nil
    end
+end
+
+---@return nil
+function M.reset_timer()
+   M.clear_timer()
    timer = vim.defer_fn(M.force_exit_insert_mode, config.idle_time_ms)
 end
 
